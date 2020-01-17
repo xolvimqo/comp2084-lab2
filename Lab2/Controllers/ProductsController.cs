363f46cb304ee44c754247b6e9af2600479c5435
Terry Gu <xolvimqo@gmail.com>
2020-01-16 20:32:58 -0500

    ASP.NET Lab2

---
+++ Lab2/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(string product)
+        {
+            ViewData["Product"] = product;
+            ViewBag.message = "You selected product: " + product;
+            return View();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Student Name: Yu-Ming(Terry) Gu
+Student Number: 200410610
